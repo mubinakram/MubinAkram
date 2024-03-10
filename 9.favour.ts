415233b87c212047bce75ff5dff2b620ae2395c3
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 17:40:54 +0500

    Create 9.favour.ts

---
+++ 9.favour.ts
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favouriteNumber = 11;
+console.log(`${favouriteNumber} is my favourite number`);
+
+
