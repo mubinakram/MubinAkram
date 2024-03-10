ac9723f9b72f576feea9b71c9f6ece4542a4f59e
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 16:49:00 +0500

    Create 7.no.8.ts

---
+++ 7.no.8.ts
@@ -0,0 +1,17 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// Addition operation
+let sum = (4 + 4);
+console.log("Addition:", sum);
+
+// Subtraction operation
+let dif = (12 - 4);
+console.log("Subtraction:", dif);
+
+// Multiplication operation
+let prod = (2 * 4);
+console.log("Multiplication:", prod);
+
+// Division operation
+let ratio = (64 / 8);
+console.log("Division:", ratio);
