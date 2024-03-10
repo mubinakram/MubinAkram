6ad559aae6e57efc9f9a6a6768be5c1a13105665
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 11:48:12 +0500

    Create 3.cases.ts

---
+++ 3.cases.ts
@@ -0,0 +1,6 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "ali";
+console.log(personName.toUpperCase());
+console.log(personName.toLowerCase());
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
