9a59b44cdd449f177c3415c527942bf346dd0ac9
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 12:23:03 +0500

    Create 4.Quote.ts

---
+++ 4.Quote.ts
@@ -0,0 +1,7 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log(`PROPHET MUHAMMAD (PBUH)," You cannot go into Jannah until you have faith, and you cannot complete your faith until you love each other."`);
+
+
+
