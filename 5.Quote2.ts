9cba1c650d3d0f01787293a5efc3630b6460a6e2
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 12:28:20 +0500

    Create 5.Quote2.ts

---
+++ 5.Quote2.ts
@@ -0,0 +1,6 @@
+(* Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message. *)
+
+let author: string = "PROPHET MUHAMMAD (PBUH)";
+let quote: string = "You cannot go into Jannah until you have faith, and you cannot complete your faith until you love each other.";
+
+console.log(`${author} once said, "${quote}"`);
