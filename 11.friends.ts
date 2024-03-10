b03aaaa2a2c5fe06c886b7936c46f5d9fb87cc43
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 20:56:57 +0500

    Create 11.friends.ts

---
+++ 11.friends.ts
@@ -0,0 +1,7 @@
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[] = ["Mubin", "Sufyan", "Ahmed", "Hasham", "Asad"];
+
+for (const name of names) {
+  console.log(name);
+}
