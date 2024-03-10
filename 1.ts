78746f0abe7eb6b74422906810c3dfebcc757ce8
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-03-10 11:14:20 +0500

    Delete 1.ts

--- 1.ts
+++
@@ -1,5 +0,0 @@
-let personName: string = "ali";
-console.log(personName.toUpperCase());
-console.log(personName.toLowerCase());
-console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
-
