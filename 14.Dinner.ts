ba130e0211665ff7037ecc346a2b3b6968b4c0d2
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-01 13:19:32 +0500

    Create 14.Dinner.ts

---
+++ 14.Dinner.ts
@@ -0,0 +1,13 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+//  like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let Guests :  string [] = ["Rehan", "Danish", "Touseef"];
+Guests.forEach(Guest => {
+    console.log(`I would like to invite you for a dinner party.It would be pleasure for me,if you come. `)
+});
+
+// OUTPUT
+
+I would like to invite you for a dinner party.It would be pleasure for me,if you come. 
+I would like to invite you for a dinner party.It would be pleasure for me,if you come. 
+I would like to invite you for a dinner party.It would be pleasure for me,if you come.
