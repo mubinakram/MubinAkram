bf13326cb07bd0eb02e23ffebf71dc159008c8f1
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-08 16:55:47 +0500

    Create 21.object.ts

---
+++ 21.object.ts
@@ -0,0 +1,23 @@
+PROBLEM:
+
+// They think of something you could store in a TypeScript Object.
+//  Write a program that creates Objects containing these items.
+
+
+let person: { name: string; age: number; city: string } = {
+    name: "Alice",
+    age: 30,
+    city: "New York"
+};
+
+// Access properties of the person object
+console.log(person.name); 
+console.log(person.age);  
+console.log(person.city); 
+
+OUTPUT:
+Alice
+30
+New York
+
+ 
