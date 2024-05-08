51c4c2240f78d3643b40289d34f1a95d9b19aff3
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-08 15:43:39 +0500

    Create 19.message.ts

---
+++ 19.message.ts
@@ -0,0 +1,14 @@
+PROBLEM:
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+//  print a message indicating the number of people you are inviting to dinner.
+
+let Guests : string[] = ["Mehmet", "Rehan", "Dansih"];
+Guests.forEach(Guest => {
+    console.log(`Dear ${Guest.length}, Welcome to our dinner party`)
+    
+});
+
+OUTPUT:
+Dear Mehmet, Welcome to our dinner party
+Dear Rehan, Welcome to our dinner party
+Dear Dansih, Welcome to our dinner party
