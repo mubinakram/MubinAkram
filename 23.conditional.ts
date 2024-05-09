3e73820d3ce3641fce5b2ac3ea8d9da5663e76a3
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-09 13:25:09 +0500

    Create 23.conditional.ts

---
+++ 23.conditional.ts
@@ -0,0 +1,76 @@
+PROBLEM:
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction
+//  for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+SOLUTION:
+
+let  mother = 'everything';
+console.log("is mother == 'everything?'I predict true.")
+console.log(mother =='everything')
+
+let book = 'friend' ;
+console.log(" is book = 'friend?'I prdict true.")
+console.log(book =='friend')
+
+let  money = 'power';
+console.log("is money == 'power?'I predict true.")
+console.log(money =='power')
+
+let brother = 'brother' ;
+console.log(" is bother = 'brother?'I prdict true.")
+console.log(brother =='brother')
+
+let success = 'beauty';
+console.log("is success == 'beauty?'I predict true.")
+console.log(success =='beauty')
+
+let  fame = 'defamed';
+console.log("is fame == 'defamed?'I predict false.")
+console.log(fame =='defamed')
+
+let love = 'lust' ;
+console.log(" is love = 'lust?'I prdict false.")
+console.log(love =='lust')
+
+let believer = 'disbeliever';
+console.log("is believer == 'disbeliever?'I predict false.")
+console.log(believer =='disbeliever')
+
+let happy = 'sad' ;
+console.log(" happy = 'sad?'I prdict false.")
+console.log(happy =='sad')
+
+let satisfied = 'unsatiefied';
+console.log("is believer == 'unsatisfied?'I predict false.")
+console.log(satisfied =='unsatisfied')
+
+OUTPUT:
+
+is mother == 'everything?'I predict true.
+true
+ is book == 'friend?'I prdict true.
+true
+is money == 'power?'I predict true.
+true
+ is bother == 'brother?'I prdict true.
+true
+is success == 'beauty?'I predict true.
+true
+is fame == 'defamed?'I predict false.
+false
+ is love == 'lust?'I prdict false.
+false
+is believer == 'disbeliever?'I predict false.
+false
+ happy == 'sad?'I prdict false.
+false
+is satisfied == 'unsatisfied?'I predict false.
+false
+
