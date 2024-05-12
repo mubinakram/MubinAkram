caf3030afe564009654b196a8870b84651070726
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-12 08:13:23 +0500

    Update 25.Alien.ts

--- 25.Alien.ts
+++ 25.Alien.ts
@@ -10,12 +10,12 @@ PROBLEM:
 OUTPUT:
 let alien_color = 'green';
 
-if (alien_color === 'green') {
+if (alien_color == 'green') {
     console.log("The player just earned 5 points.");
 }
 
   alien_color = "red";
-if (alien_color === "green") {
+if (alien_color == "green") {
     console.log("The player just earned 5 points."
     );
 };
