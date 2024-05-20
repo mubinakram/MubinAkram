f5d1f72d76186998982d30d9bab1afc39d0df76a
mubinakram <162419828+mubinakram@users.noreply.github.com>
2024-05-20 07:12:26 +0500

    Create 41.magician.ts

---
+++ 41.magician.ts
@@ -0,0 +1,18 @@
+function show_magicians(magicians: string[]): void {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+
+// Array of magician's names
+const magician_names: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+
+// Call the function to show magician's names
+show_magicians(magician_names);
+
+output:
+
+Harry 
+ahmed
+Hassan
+Teller
